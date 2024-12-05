3281e4f1ba186071649b08cbe0cd35521474eca5
papabn <pndiaye999@gmail.com>
2024-12-05 14:45:54 +0000

    setup tailwind css and shadcnUI

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-4xl text-red-500'>Hello World</p>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
